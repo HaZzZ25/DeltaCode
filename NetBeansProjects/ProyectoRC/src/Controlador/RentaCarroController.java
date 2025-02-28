4d0ab9b179d60d46481ce1f3d0ffc4b4cf42c714
hazel <hazel@fedora>
2025-02-27 22:54:53 -0600

    Inicio de poyecto

---
+++ NetBeansProjects/ProyectoRC/src/Controlador/RentaCarroController.java
@@ -0,0 +1,30 @@
+package Controlador;
+
+import java.awt.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author hazel
+ */
+public class RentaCarroController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private Button btnHola;
+
+}
+
+    
+
